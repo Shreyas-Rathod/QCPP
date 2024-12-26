f8081d0470eb1854bb073f9c5ea4f2ca96524061
Shreyas Rathod <sanskrit.112358@gmail.com>
2024-12-26 16:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 87.1 MB (45.48%) - LeetSync

--- 790-global-and-local-inversions/global-and-local-inversions.cpp
+++ 790-global-and-local-inversions/global-and-local-inversions.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     bool isIdealPermutation(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0;i<n;i++){
-            if(i-nums[i] > 1 || i-nums[i] < -1) return false;
-        }
+        for(int i = 0;i<n;i++) 
+            if(abs(i-nums[i])>1) return false;
         return true;
     }
 };
