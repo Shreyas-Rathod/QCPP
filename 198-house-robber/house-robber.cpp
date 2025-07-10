e75f8b391f670127ed98d06d8f7437ca84f51254
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-07-10 15:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,14 +3,16 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int>res(n, 0);
-        res[0] = nums[0];
-        res[1] = max(nums[1], res[0]);
+        int res0 = nums[0];
+        int res1 = max(nums[1], res0);
+        int newn = 0;
 
         for(int i = 2;i<n;i++){
-            res[i] =  max(res[i-1], nums[i] + res[i-2]);
+            newn = max(res1, nums[i] + res0);
+            res0 = res1;
+            res1 = newn;
         }
         
-        return max(res[n-1], res[n-2]);
+        return max(res0, res1);
     }
 };
