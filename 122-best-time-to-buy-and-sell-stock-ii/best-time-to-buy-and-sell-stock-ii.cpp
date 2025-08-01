e5b6bb1a6b4186435c4af8d17a442f695366d076
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-08-01 16:40:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (70.48%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -6,11 +6,11 @@ public:
         prev[0] = prev[1] = 0;
 
         for(int i = n-1;i>=0;i--){
-            curr[0] = max(prev[1] - prices[i], prev[0]);
-            curr[1] = max(prev[0] + prices[i], prev[1]);
+            curr[0] = max(prices[i] + prev[1], prev[0]);
+            curr[1] = max(prev[0] - prices[i], prev[1]);
             prev = curr;
         }
 
-        return curr[0];
+        return curr[1];
     }
 };
