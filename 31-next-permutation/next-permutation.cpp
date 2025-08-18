9346a6882a741c3cb83d9bf38e96b358e428932c
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-08-18 10:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.63%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(), i = n-2;
-        while(i >= 0){
-            if(nums[i] >= nums[i+1]) i--;
-            else{
-                int j = n-1;
-                while(i!=j && nums[i] >= nums[j]) j--;
-                swap(nums[i], nums[j]);
-                sort(nums.begin()+i+1, nums.end());
-                break;
-            }
+        int n = (int)nums.size();
+        if (n <= 1) return;
+
+        // 1) Find first index i from the right where nums[i] < nums[i+1]
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+        if (i >= 0) {
+            // 2) Find rightmost j > i with nums[j] > nums[i]
+            int j = n - 1;
+            while (j > i && nums[j] <= nums[i]) j--;
+            // 3) Swap pivot with that element
+            swap(nums[i], nums[j]);
         }
-        if(i != -1) return;
-        reverse(nums.begin(), nums.end());
+
+        // 4) Reverse the suffix to get the smallest arrangement
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
