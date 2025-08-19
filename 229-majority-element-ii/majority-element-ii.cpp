3ce5ec82acf8ab3813bbbac6e4d3c0909cb3f59a
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-08-19 17:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (99.81%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& v) {
-        int n = v.size(), c1vote = 0, c2vote = 0, candi1 = INT_MIN, candi2 = INT_MIN;
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt1 = 0, cnt2 = 0, candi1 = INT_MIN, candi2 = INT_MIN;
+        for(int i = 0;i<n;i++){
+            if(candi1 == nums[i]) cnt1++;
+            else if(candi2 == nums[i]) cnt2++;
+            else if(cnt1 == 0) candi1 = nums[i], cnt1++;
+            else if(cnt2 == 0) candi2 = nums[i], cnt2++;
+            else cnt1--, cnt2--; 
+        }
+
         int c1 = 0, c2 = 0;
+        for(int x : nums){
+            if(x == candi1) c1++;
+            if(x == candi2) c2++;
+        }
+
         vector<int>res;
-
-        for(int i = 0;i<n;i++){
-            if(c1vote == 0 && candi2 != v[i]) candi1 = v[i], c1vote = 1;
-            else if(c2vote == 0 && candi1 != v[i]) candi2 = v[i], c2vote = 1;
-            else{
-                if(candi1 == v[i]) c1vote++;
-                else if(candi2 == v[i]) c2vote++;
-                else c1vote--, c2vote--;
-            }
-        }
-        for(int i = 0;i<n;i++){
-            if(candi1 == v[i]) c1++;
-            if(candi2 == v[i]) c2++;
-        }
-
         if((int)(n/3) < c1) res.push_back(candi1);
         if((int)(n/3) < c2) res.push_back(candi2);
+
         return res;
     }
 };
