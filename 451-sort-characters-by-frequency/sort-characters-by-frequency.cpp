7453774134cafca317ea42160f17c140c75f2569
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-08-29 17:04:27 +0530

    Time: 10 ms (8.41%) | Memory: 11.4 MB (44.81%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-
-    struct sortstruct{
-        typedef pair<char, int> type;
-        bool operator()(type const&a, type const&b)const{
-            return a.second<b.second;
-        }
-    };
     string frequencySort(string s) {
+        int n = s.size();
         map<char, int>mp;
-        int n = s.size(), mpss;
-        string res;
-
-        for(int i = 0;i<n;i++) mp[s[i]]++;
-
-        vector<pair<char, int>>mpcy(mp.begin(), mp.end());
-        sort(mpcy.rbegin(), mpcy.rend(), sortstruct());
-        mpss = mpcy.size();
-        for(int i = 0;i<mpss;i++){
-            for(int j = 0;j<mpcy[i].second;j++) res.push_back(mpcy[i].first);
-        }       
+        for(char x : s) {
+            if(x != ' ') mp[x]++;
+        }
+        vector<pair<char, int>>mpss(mp.begin(), mp.end());
+        sort(mpss.begin(), mpss.end(), [](auto &a, auto &b){
+            return a.second > b.second;
+        });
+        string res = "";
+        for(auto &p : mpss){
+            res.append(p.second, p.first);
+        }
 
         return res;
     }
