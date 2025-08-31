8cee447c13c1b8f5b2dc5af5f5cc682f3d2491fd
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-08-31 23:20:19 +0530

    Time: 3 ms (14.41%) | Memory: 10.3 MB (5.02%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int recur(string s, int i, int sign, long long res){
+    int fun(string s, int i, int sign, long long res){
         if(i >= s.size() || !isdigit(s[i])) return res*sign;
         res = res*10 + s[i] - '0';
-        if(res*sign <= INT_MIN) return INT_MIN;
         if(res*sign >= INT_MAX) return INT_MAX;
-        return recur(s, i+1, sign, res);
-    }
+        if(res*sign <= INT_MIN) return INT_MIN;
+        return fun(s, i+1, sign, res);
+    };
     int myAtoi(string s) {
-
-        size_t start = s.find_first_not_of(" ");
-        if(start == string::npos) return 0;
-        s = s.substr(start);
+        size_t i = s.find_first_not_of(" ");
+        if(i == string::npos) return 0;
+        s = s.substr(i);
         if(s.empty()) return 0;
-        int sign = 1, index = 0;
-        if(s[0] == '-') sign = -1, index = 1;
-        else if(s[0] == '+') index = 1; 
-        return recur(s, index, sign, 0);
+
+        int index = 0, sign = 1;
+        if(s[0] == '-') sign = -1, index++;
+        else if(s[0] == '+') index++;
+
+        return fun(s, index, sign, 0);
     }
 };
