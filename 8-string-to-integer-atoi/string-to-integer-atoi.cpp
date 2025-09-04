3f351880553267514d8ddd0b9a2dab570970d02f
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-05 04:56:30 +0530

    Time: 2 ms (18.62%) | Memory: 10.5 MB (5.35%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int fun(string s, int i, int sign, long long res){
-        if(i >= s.size() || !isdigit(s[i])) return res*sign;
-        res = res*10 + s[i] - '0';
-        if(res*sign >= INT_MAX) return INT_MAX;
+    int fun(string s, int index, int sign, long long res){
+        if(index >= s.size() || !isdigit(s[index])) return res*sign;
+        res = res*10 + s[index] - '0';
         if(res*sign <= INT_MIN) return INT_MIN;
-        return fun(s, i+1, sign, res);
-    };
+        if(res*sign >= INT_MAX) return INT_MAX; 
+
+        return fun(s, index + 1, sign, res);
+
+    }
     int myAtoi(string s) {
         size_t i = s.find_first_not_of(" ");
-        if(i == string::npos) return 0;
+        if(i == string :: npos) return 0;
         s = s.substr(i);
         if(s.empty()) return 0;
 
         int index = 0, sign = 1;
-        if(s[0] == '-') sign = -1, index++;
-        else if(s[0] == '+') index++;
+        if(s[0] == '+') index++;
+        else if(s[0] == '-') index++, sign = -1;
 
         return fun(s, index, sign, 0);
+
     }
 };
