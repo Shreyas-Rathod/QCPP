692b55e064942408605ea7d3d1db725a78525745
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-05 19:40:05 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (82.81%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void genfun(vector<int>&candi, int target, int i, vector<vector<int>>&res, vector<int>&temp){
-        if(target == 0){
-            res.push_back(temp);
-            return;
+    void findcomb(vector<int>&cand, int k, int csum, int idx, vector<vector<int>>&res, vector<int>&cc){
+        int prev = -1;
+        for(int i = idx;i<cand.size();i++){
+            if(csum + cand[i] > k) break;
+            if(prev == cand[i]) continue;
+            cc.push_back(cand[i]);
+            findcomb(cand, k, csum + cand[idx], i+1, res, cc);
+            cc.pop_back();
+            prev = cand[i];
         }
-        for(int k = i;k<candi.size();k++){
-            if(k>i && candi[k]==candi[k-1]) continue;
-            if(candi[k]>target) break;
-            temp.push_back(candi[k]);
-            genfun(candi, target-candi[k], k+1, res, temp);
-            temp.pop_back();
-        }
-        
+        if(accumulate(cc.begin(), cc.end(), 0) == k) res.push_back(cc);
     }
-    vector<vector<int>> combinationSum2(vector<int>& candi, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>>res;
-        vector<int>temp;
-        sort(candi.begin(), candi.end());
-        genfun(candi, target, 0, res, temp);
+        vector<int>cc;
+        findcomb(candidates, target, 0, 0, res, cc);
         return res;
     }
 };
