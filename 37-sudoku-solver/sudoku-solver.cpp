e1e16409267c97921bbdd638a07e0cb67666fbc2
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-08 15:52:42 +0530

    Time: 375 ms (41.56%) | Memory: 8.6 MB (99.43%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -7,14 +7,14 @@ public:
         }
         return true;
     }
-    bool solve(vector<vector<char>>&board){
-        for(int i = 0;i<board.size();i++){
-            for(int j = 0;j<board.size();j++){
+    bool fun(vector<vector<char>>&board){
+        for(int i = 0;i<9;i++){
+            for(int j = 0;j<9;j++){
                 if(board[i][j] == '.'){
                     for(char k = '1';k<='9';k++){
                         if(isvalid(board, i, j, k)){
                             board[i][j] = k;
-                            if(solve(board)) return true;
+                            if(fun(board)) return true;
                             else board[i][j] = '.';
                         }
                     }
@@ -25,7 +25,7 @@ public:
         return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        fun(board);
         return;
     }
 };
