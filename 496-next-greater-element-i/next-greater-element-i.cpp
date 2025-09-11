c7ecc2c4ddc764789412b981a760f5dae52bb284
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-11 16:23:37 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (15.36%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int>res(m, -1);
+        int n1 = nums1.size(), n2 = nums2.size();
+        vector<int>res(n2, -1);
         stack<int>st;
-        for(int i = m-1;i>=0;i--){
-            if(st.empty()) {st.push(nums2[i]); continue;}
+
+        for(int i = n2-1;i>=0;i--){
+            if(st.empty()){st.push(nums2[i]); continue;}
             while(!st.empty() && nums2[i] > st.top()) st.pop();
             if(!st.empty()) res[i] = st.top();
             st.push(nums2[i]);
         }
+
         map<int, int>mp;
-        for(int i = 0;i<m;i++) mp[nums2[i]] = res[i];
+        for(int i = 0;i<n2;i++) mp[nums2[i]] = res[i];
         res.clear();
-        for(int i = 0;i<n;i++) res.push_back(mp[nums1[i]]);
+        for(int i = 0;i<n1;i++) res.push_back(mp[nums1[i]]);
+
         return res;
+
     }
 };
