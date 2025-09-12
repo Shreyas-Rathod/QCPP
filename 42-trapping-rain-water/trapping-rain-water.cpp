d7bf4d6057867773497615e6ef64ec6bf3862446
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-12 14:43:10 +0530

    Time: 1 ms (25.48%) | Memory: 27.4 MB (20.04%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int count = 0;
-        vector<int>suffix(n), prefix(n);
+        int n = height.size(), count = 0;
+        vector<int>next(n, 0);
+        vector<int>prev(n, 0);
+        prev[0] = height[0];
+        next[n-1] = height[n-1];
 
-        prefix[0] = height[0];
-        for(int i = 1;i<n;i++) prefix[i] = max(prefix[i-1], height[i]);
-        suffix[n-1] = height[n-1];
-        for(int i = n-2;i>=0;i--) suffix[i] = max(suffix[i+1], height[i]);
-        for(int i = 0;i<n;i++) count += min(suffix[i], prefix[i]) - height[i];
+        for(int i = 0;i<n;i++){
+            if(i > 0) prev[i] = max(prev[i-1], height[i]);
+            if(n-1 > i) next[n-i-2] = max(next[n-i-1], height[n-i-2]);
+        }
+
+        for(int i = 0;i<n;i++){
+            count += min(prev[i], next[i])-height[i];
+        }
+
         return count;
     }
 };
