eb244c0b12a02a0c7b6226844e27db19d881f958
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-12 16:01:21 +0530

    Time: 11 ms (89.15%) | Memory: 47 MB (89.66%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -4,13 +4,12 @@ public:
         int n = arr.size(), mod = 1e9 + 7;
         long left, right, count = 0;
         stack<int>st;
-        vector<int>pre(n, 0);
-        vector<int>nxt(n, 0);
+        vector<int>pre(n, -1);
+        vector<int>nxt(n, n);
 
         for(int i = n-1;i>=0;i--){
             while(!st.empty() && arr[i] <= arr[st.top()]) st.pop();
             if(!st.empty()) nxt[i] = st.top();
-            else nxt[i] = n;
             st.push(i); 
         }
 
@@ -19,7 +18,6 @@ public:
         for(int i = 0;i<n;i++){
             while(!st.empty() && arr[i] < arr[st.top()]) st.pop();
             if(!st.empty()) pre[i] = st.top();
-            else pre[i] = -1;
             st.push(i); 
         }
 
