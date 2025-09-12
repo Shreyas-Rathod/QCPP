3870becab38e7bf7db6ebd40df6e2674fd2841d1
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-12 16:00:27 +0530

    Time: 11 ms (89.15%) | Memory: 47 MB (81.29%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int count = 0, n = arr.size(),mod = 1e9+7;
-        long long left, right;
-        vector<int>pse(n, 0), nse(n, 0);
+        int n = arr.size(), mod = 1e9 + 7;
+        long left, right, count = 0;
         stack<int>st;
+        vector<int>pre(n, 0);
+        vector<int>nxt(n, 0);
+
         for(int i = n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            nse[i] = st.empty() ? n : st.top();
-            st.push(i);
+            while(!st.empty() && arr[i] <= arr[st.top()]) st.pop();
+            if(!st.empty()) nxt[i] = st.top();
+            else nxt[i] = n;
+            st.push(i); 
         }
+
         while(!st.empty()) st.pop();
+
         for(int i = 0;i<n;i++){
-            while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
-            pse[i] = st.empty() ? -1 : st.top();
-            st.push(i);
+            while(!st.empty() && arr[i] < arr[st.top()]) st.pop();
+            if(!st.empty()) pre[i] = st.top();
+            else pre[i] = -1;
+            st.push(i); 
         }
+
         for(int i = 0;i<n;i++){
-            left = i - pse[i];
-            right = nse[i] - i;
-            count = (count + (arr[i]*(left*right)%mod)%mod)%mod;
+            left = i - pre[i];
+            right = nxt[i] - i;
+            count = (count + (arr[i]*(right*left)%mod)%mod)%mod;
         }
+
         return count;
+
     }
 };
