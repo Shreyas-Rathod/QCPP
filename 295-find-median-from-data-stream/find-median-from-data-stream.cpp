f74d64106a8666ed0090e0c0f6264824e0c1109f
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-09-19 15:39:04 +0530

    Time: 83 ms (35.52%) | Memory: 151 MB (5.97%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -0,0 +1,30 @@
+class MedianFinder {
+public:
+    priority_queue<long>maxheap;
+    priority_queue<long, vector<long>, greater<long>>minheap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        maxheap.push(num);
+        minheap.push(maxheap.top());
+        maxheap.pop();
+        if(maxheap.size() < minheap.size()){
+            maxheap.push(minheap.top());
+            minheap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxheap.size() == minheap.size()) return (maxheap.top() + minheap.top())/2.0;
+        else return maxheap.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
