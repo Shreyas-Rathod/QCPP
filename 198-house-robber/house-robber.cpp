87eb6ceb8664761a173eae055cb14f906fbba3e2
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-10-04 11:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (80.45%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int cob(vector<int>& nums, int st, int end){
-        int n = end - st + 1;
-        if(n == 1) return nums[st];
-        int res0 = nums[st];
-        int res1 = max(nums[st+1], res0);
-        int newn = 0;
-
-        for(int i = st + 2;i<=end;i++){
-            newn = max(res1, nums[i] + res0);
+    int rob(vector<int>& nums) {
+        int n = nums.size(), taken, nottaken, res0, res1;
+        res0 = nums[0];
+        if(n == 1) return res0;
+        res1 = max(res0, nums[1]);
+        for(int i = 2;i<n;i++){
+            taken = nums[i] + res0;
             res0 = res1;
-            res1 = newn;
+            res1 = max(taken, res0);
         }
 
-        return max(res0, res1);
-    }
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        return cob(nums, 0, n-1);
+        return res1;
     }
 };
