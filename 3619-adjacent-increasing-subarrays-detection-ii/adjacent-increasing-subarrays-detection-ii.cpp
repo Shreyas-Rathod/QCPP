fe3b762ce69027aa3521fd5eacaf9f33e2cedd6f
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-10-15 19:27:25 +0530

    Time: 209 ms (89.85%) | Memory: 173.7 MB (81.58%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    bool check(vector<int>&nums, int k){
+    int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        if(2*k > n) return false;
-        int prev_cnt = 0, curr_cnt = 1;
+        int prev_cnt = 0, curr_cnt = 1, res = 0;
         for(int i = 1;i<n;i++){
             if(nums[i] > nums[i-1]) curr_cnt++;
             else prev_cnt = curr_cnt, curr_cnt = 1;
-            if((curr_cnt >= k && prev_cnt >= k) || curr_cnt >= 2*k) return true;
-        }
-        return false;
-    }
-
-    int maxIncreasingSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        int low = 1, high = n, mid;
-        while(low <= high){
-            mid = (low + high) >> 1;
-            if(check(nums, mid)) low = mid + 1;
-            else high = mid - 1;
+            res = max(res, max(curr_cnt >> 1, min(prev_cnt, curr_cnt)));
         }
 
-        return high;
+        return res;
     }
 };
